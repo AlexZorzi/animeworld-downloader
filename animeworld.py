023b53f04a2254959eddc9ad240c99862de99af3
gabryon99 <leleattolo99@gmail.com>
2019-09-05 21:07:30 +0200

    added new features

--- animeworld.py
+++ animeworld.py
@@ -6,14 +6,12 @@ import re
 import os
 import argparse
 import json
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import wget
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
-
-from enum import Enum
     
 
 class Anime():
@@ -23,20 +21,20 @@ class Anime():
         self.year = year
         self.link = link_id
         self.id = aid
-
-    def __str__(self):
+    def __str__(self) -> str:
         return f"[{self.id}] - {self.title}"
 
 
-def print_log(m: str, key: str = "none") -> None:
-    emojies = {
-        'cat': 'ㅇㅅㅇ',
-        'confused': '（・∩・）',
-        'angry': '(¬､¬)',
-        'happy': '（＞ｙ＜）',
-        'none': ''
-    }
-    print("[{}] ene-chan: {} {}".format(str(datetime.date.today().strftime("%d/%m/%Y - %h")), m, emojies[key]))
+def handle_input(message: str) -> None:
+    try:
+        return input(message)
+    except KeyboardInterrupt:
+        print_log("chiusura del programma in corso...", '\n')
+        exit(1)
+        return
+
+def print_log(m: str, key: str = "") -> None:
+    print("{}[{}] animeworld-downloader: {}".format(key, str(datetime.date.today().strftime("%d/%m/%Y")), m))
     return 
 
 def create_directory(anime_name: str) -> str:
@@ -56,15 +54,18 @@ def create_directory(anime_name: str) -> str:
     
     return None
 
-def get_episode_ids(server: str) -> List:
+def get_episodses(server: str) -> List:
 
     episodes_id = list()
     for link in server.find_all('a'):
         if 'data-id' in link.attrs:
-            episodes_id.append(link.attrs['data-id'])
+            episodes_id.append({
+                'id': link.attrs['data-id'],
+                'number': int(link.attrs['data-base'])
+            })
     return episodes_id
 
-def download_anime(url_anime_raw: str) -> None:
+def download_anime_by_link(url_anime_raw: str, range: list) -> None:
 
     html_page = requests.get(url_anime_raw).text
     soup = BeautifulSoup(html_page, "html.parser")
@@ -72,14 +73,14 @@ def download_anime(url_anime_raw: str) -> None:
     h1 = soup.findAll("h1", {"class": "title"})
     title = h1[0].text
 
-    print_log("Contatto il sever di animeworld.tv per scaricare' %s'..." % title)
+    print_log("contatto il sever di animeworld.tv per scaricare' %s'..." % title)
 
     server_28 = soup.find('div', attrs={
         'data-id': 28,
         'class': 'server'
     })
 
-    episodes_id = get_episode_ids(server_28)
+    episodes = get_episodses(server_28)
     url_request = 'https://www.animeworld.tv/ajax/episode/info'
     headers = {
         'accept': 'application/json, text/javascript, */*; q=0.01',
@@ -91,20 +92,32 @@ def download_anime(url_anime_raw: str) -> None:
     links_episode = list()
     i = 0
 
-    for anime_id in episodes_id:
-        query = {'ts': int(time.time()), 'server': 28, 'id': anime_id, '_': 674}
-        print_log("Richiesta al server 28 per l'episodio %s..." % anime_id)
+    for anime in episodes:
+        
+        anime_id = anime['id']
+        anime_num = anime['number']
+        if len(range) >= 2:
+            if not anime_num in range:
+                continue            
+    
+        print_log("richiesta al server 28 per l'episodio [{}/{}]...".format(anime_id, anime_num))
+        
+        query = {'ts': int(time.time()), 'server': 28, 'id': anime_id, '_': 674}        
         r = requests.get(url_request, params=query, headers=headers)
         data = r.json()
-        print_log("Risposta per l'episodio %s ricevuta correttamente dal server 28!" % anime_id)
+
+        print_log(f"risposta per l'episodio [{anime_id}/{anime_num}] ricevuta correttamente dal server 28!")
+
         link_episode = data['grabber']
         links_episode.append(link_episode)
+
         i += 1
-        print_log("Trovati attualmente %d episodio/i..." % i)
+
+        print_log("trovati attualmente %d episodio/i..." % i)
     
     path = create_directory(title)
-    print_log("Creata directory al seguente indirizzo: '%s'" % path)
-    print_log("In download %d episodi dell'anime '%s':" % (len(links_episode), title))
+    print_log("creata directory al seguente indirizzo: '%s'" % path)
+    print_log("in download %d episodi dell'anime '%s':" % (len(links_episode), title))
 
     i = 0
 
@@ -115,7 +128,7 @@ def download_anime(url_anime_raw: str) -> None:
         episode_path = "{}/{}".format(path, regex[0])
 
         if os.path.isfile(episode_path):
-            print_log("Questo episodio è già stato scaricato!")
+            print_log("questo episodio è già stato scaricato!")
             i += 1 
             continue
 
@@ -123,9 +136,9 @@ def download_anime(url_anime_raw: str) -> None:
             wget.download(link, episode_path)
             print("\n")
         except KeyboardInterrupt:
-            print_log("\n[X] Error: download dell'anime annullato come richiesto dall'utente...")
-            print_log("I file creati verranno eliminati dal sistema...")
-            print_log("Chiusura del programma...")
+            print_log("\n[!] Warning: download dell'anime annullato come richiesto dall'utente...")
+            # TODO: clean files from system
+            print_log("chiusura del programma in corso...")
             exit(1)
         pass
 
@@ -143,32 +156,79 @@ def search_by_keyword(keyword: str) -> Dict[int, Anime]:
     """
     def make_request(key: str) -> str:
         request = requests.get('https://www.animeworld.tv/ajax/film/search', params={'keyword': key })
-        answer = request.content.decode('utf8').replace("'", '"')
-        data = json.loads(answer)
+        data = request.json()
         return data['html']
 
+    data = make_request(keyword)
+    if data == None:
+        return {}
+
     # create an html parse to elaborate searches
-    soup = BeautifulSoup(make_request(keyword), 'html.parser')
+    soup = BeautifulSoup(data, 'html.parser')
 
     # get anime links and titles, and assign an id
     a_id = 1
     anime_list = dict()
 
-    for e in soup.find_all('a'):
+    for a_tag in soup.find_all('a'):
 
         # get title
-        title = e.get('data-jtitle')
+        title = a_tag.get('data-jtitle')
         # if title is null then there isn't any anime in that a element
         if title != None:
-            link = "{}/{}".format("https://www.animeworld.tv", e.get('href'))
+            link = "{}/{}".format("https://www.animeworld.tv", a_tag.get('href'))
             anime_list[a_id] = Anime(title, 'None', 2019, link, a_id)
             a_id += 1
             
     return anime_list
 
-def search_anime():
+def get_anime_range() -> List:
     
-    anime_name = input("Digita il nome di un anime che vuoi cercare: ")
+    print("Quale episodio/i vuoi scaricare? Digita un range nella forma 'xxx:yyy'.")
+    print("Altrimenti, se vuoi scaricare l'intera serie digita 'all'")
+    print("Oppure se vuoi scaricare episodi specifici usa la seguente forma x;y;z;k")
+
+    is_valid = False
+
+    episodes = []
+
+    while not is_valid:
+        user_input = handle_input('> ')
+        if user_input.lower() == 'all':
+            # do something
+            episodes = ['all']
+            is_valid = True
+        else:
+            # check for ; pattern
+            if re.search("^[0-9]+(\;|\n)", user_input):
+                is_valid = True
+                episodes = re.split(';', user_input)
+                episodes = list(map(int, episodes))
+            elif re.search("^[0-9]+\:[0-9]+", user_input):
+                is_valid = True
+                temp = re.split(':', user_input)
+                for i in range(int(temp[0]), int(temp[1]) + 1):
+                    episodes.append(i)
+            else:
+                print("Pattern inserito non valido! Riprova...")
+                is_valid = False
+
+    print(episodes)
+
+    return episodes
+
+def download_anime() -> None:
+
+    anime_link = ''
+    anime_link = handle_input("Inserisci il link dell'anime che vuoi scaricare: ")
+    episodes = get_anime_range()
+    download_anime_by_link(anime_link, episodes)
+
+    return
+
+def search_anime() -> None:
+    
+    anime_name = handle_input("Digita il nome di un anime che vuoi cercare: ")
     if anime_name is None:
         print("[X] Il nome inserito non e' valido!")
         return
@@ -185,7 +245,7 @@ def search_anime():
         choiced = -1
 
         while choiced < 0 or choiced >= len(anime.values()) + 1:
-            user_input = input(f"Digita un numero tra [1-{len(anime.values())}]: ")
+            user_input = handle_input(f"Digita un numero tra [1-{len(anime.values())}] (0 per uscire): ")
             try:
                 choiced = int(user_input)
             except ValueError:
@@ -195,12 +255,14 @@ def search_anime():
             return
         anime_choiced = anime[choiced]
         print(f"Preparazione del download dell'anime: '{anime_choiced.title}'")
-        download_anime(anime_choiced.link)
+        episodes = get_anime_range()
+        print("adesso attendi un istante...")
+        download_anime_by_link(anime_choiced.link, episodes)
 
     else:
         print("Non e' stato trovato nessun anime col nome inserito.")
 
-def main():
+def main() -> int:
 
     def setup_parser() -> argparse.ArgumentParser:
         parser = argparse.ArgumentParser(description="Inserisci il link dell'anime che vuoi scaricare.")
@@ -214,25 +276,27 @@ def main():
         print("3 - Esci dal programma")
 
     parser = setup_parser()
+    arg_link = parser.parse_args().link
+
+    if arg_link != '':
+        download_anime_by_link(arg_link)
+        return 0
+
     user_choice = 0
     menu = [
-        search_anime
-
+        search_anime,
+        download_anime
     ]
 
-    while user_choice != 3:
+    while user_choice != len(menu) + 1:
         print_menu()
-        try:
-            user_choice = input('> ')
-        except KeyboardInterrupt:
-            print("Chiusura del programma in corso...")
+        user_choice = handle_input('> ')
         if user_choice == 3:
             continue
         menu[int(user_choice) - 1]()
         print("")
 
+    return 0
 
-    return
-
-
+# execute program
 main()
